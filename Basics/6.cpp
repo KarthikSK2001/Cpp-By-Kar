696cfbc456e71e5880f264c1030d85ef616d7bb7
Karthik S <89517611+KarthikSK2001@users.noreply.github.com>
2022-08-30 20:36:07 +0530

    Create 6.cpp

---
+++ Basics/6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a,b;
+     cin>>a>>b;
+     cout<<"Before Swapping a = "<<a<<" b = "<<b<<endl;
+     int temp=a;
+     a=b;
+     b=temp;
+     cout<<"After Swapping a = "<<a<<" b = "<<b<<endl;
+     
+}
