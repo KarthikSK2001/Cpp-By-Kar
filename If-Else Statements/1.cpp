90ad86d9d219930f67e4a680a4eaa741390208c1
Karthik S <89517611+KarthikSK2001@users.noreply.github.com>
2022-08-30 20:44:07 +0530

    Create 1.cpp

---
+++ If-Else Statements/1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a==0)
+        cout<<"Number is Zero!"<<endl;
+    else if(a%2==0)
+        cout<<"Number is Even"<<endl;
+    else 
+        cout<<"Number is Odd!"<<endl;
+        
+}
