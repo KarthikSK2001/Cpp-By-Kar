bc160afb7ccd5c7b9d7fe9cb5ace8e3ca017e851
Karthik S <89517611+KarthikSK2001@users.noreply.github.com>
2022-08-30 20:31:15 +0530

    Create 4.cpp

---
+++ Basics/4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a,b,quotient;
+     int remainder;
+     cin>>a>>b;
+     quotient=a/b;
+     remainder=a%b;
+     cout<<"Quotient is "<<quotient<<endl;
+     cout<<"Remainder is "<<remainder<<endl;
+}
