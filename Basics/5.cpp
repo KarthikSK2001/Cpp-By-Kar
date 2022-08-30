959dac4d35140d688ab1c69affae8692864469e4
Karthik S <89517611+KarthikSK2001@users.noreply.github.com>
2022-08-30 20:33:29 +0530

    Create 5.cpp

---
+++ Basics/5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a;
+     float b;
+     double c;
+     char d;
+     cout<<"Size of Integer is = "<<sizeof(a)<<endl;
+     cout<<"Size of Float is = "<<sizeof(b)<<endl;
+     cout<<"Size of Double is = "<<sizeof(c)<<endl;
+     cout<<"Size of Character is = "<<sizeof(d)<<endl;
+}
